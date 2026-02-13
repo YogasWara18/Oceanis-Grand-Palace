347b13aafb268c96541a6ca5aea8f38966eb0ef5
YogasWara18 <teukukurniayogaswara@gmail.com>
2026-02-14 01:33:43 +0700

    Update about

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import CountUp from 'react-countup';
+import authorImg from "../../assets/about.jpg";
+import signatureImg from "../../assets/Signature.png";
+
+import heroImg from "../../assets/Hero-about2.png";
+import { Link } from "react-router-dom";
+
+
+
 
 function About() {
   return (
     <>
-      about
+      <div className="section-banner mt-5 pt-6 relative flex"></div>
     </>
   )
 }
